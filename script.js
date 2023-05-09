2ea98228c104b0a677f7b12be1f604a1bbd0a1e8
Chris <chriscablish@hotmail.com>
2023-05-09 14:57:34 -0500

    minutes funcitnality addeda

--- script.js
+++ script.js
@@ -1,55 +1,74 @@
 const millisecondsDiplay = document.getElementById('milliseconds');
 const secondsDisplay = document.getElementById('seconds');
+const minutesDisplay = document.getElementById('minutes');
 
 const start = document.getElementById('start');
 const stop = document.getElementById('stop');
 const reset = document.getElementById('reset');
 
 let intervalID;
-let startTime = 0;
-let elapsedTime = 0;
-let seconds = 0;
-let lastLoggedTime = 0;
+let startTime = 0; //time (in ms) that start button was clicked
+let millisecondsSinceStart = 0; //amount of milliseconds since start
+let lastLoggedSeconds = 0;
+let lastLoggedMinutes = 0; 
+let currentTimeInSeconds;
+let currentTimeInMinutes;
 
-//START BUTTON EVENT
 start.addEventListener('click', () => {
-    // If the timer is not running, set the start time to the current time
     if (!intervalID) {
-        startTime = new Date().getTime() - elapsedTime;
+        startTime = new Date().getTime() - millisecondsSinceStart;
         intervalID = setInterval (() => {
-            elapsedTime = new Date().getTime() - startTime;
-            millisecondsDiplay.innerText = elapsedTime;
-            
-            const currentTimeInSeconds = Math.floor(elapsedTime / 1000);
-            if (currentTimeInSeconds > lastLoggedTime) {
+            millisecondsSinceStart = new Date().getTime() - startTime;
+            millisecondsDiplay.innerText = millisecondsSinceStart;
+
+            currentTimeInSeconds = Math.floor(millisecondsSinceStart / 1000);
+            if (currentTimeInSeconds > lastLoggedSeconds) {
                 secondsDisplay.innerText = currentTimeInSeconds;
-                console.log('action');
-                lastLoggedTime = currentTimeInSeconds;
+            }
+            currentTimeInMinutes = Math.floor(currentTimeInSeconds / 60);
+            if (currentTimeInMinutes > lastLoggedMinutes) {
+                minutesDisplay.innerText = currentTimeInMinutes;
             }
 
-        }, 1)
+        }, 1);
     }
 });
 
-//STOP BUTTON EVENT
+
+
+
+
+
+
+
+// start.addEventListener('click', () => {
+//     if (!intervalID) {
+//         startTime = new Date().getTime() - elapsedTime;
+//         intervalID = setInterval (() => {
+//             elapsedTime = new Date().getTime() - startTime;
+//             millisecondsDiplay.innerText = elapsedTime;
+            
+//             const currentTimeInSeconds = Math.floor(elapsedTime / 1000);
+//             if (currentTimeInSeconds > lastLoggedTime) {
+//                 secondsDisplay.innerText = currentTimeInSeconds;
+//                 lastLoggedTime = currentTimeInSeconds;
+//             }
+
+//         }, 1)
+//     }
+// });
+
+
 stop.addEventListener('click', () => {
     clearInterval(intervalID);
-    intervalID = null; // set the intervalID to null to indicate that the timer is not running
+    intervalID = null; 
 });
 
 
 reset.addEventListener('click', () => {
-    elapsedTime = 0;
-    millisecondsDiplay.innerText = elapsedTime;
-})
-
-
-
-
-
-
-
-
-
-
+    millisecondsSinceStart = 0;
+    millisecondsDiplay.innerText = millisecondsSinceStart;
+    currentTimeInSeconds = Math.floor(millisecondsSinceStart / 1000);
+    secondsDisplay.innerText = currentTimeInSeconds;
+});
 
