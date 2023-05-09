50d177e5fc02d8a123ce496d867664b9bb714754
Chris <chriscablish@hotmail.com>
2023-05-09 09:37:02 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Start button is clicked
+
+start time variable is defined by getting time from date object. 
+
+
+setInterval() function happens every 1 ms.
+
---
+++ script.js
@@ -0,0 +1,45 @@
+const millisecondsDiplay = document.getElementById('milliseconds');
+const secondsDisplay = document.getElementById('seconds');
+
+const start = document.getElementById('start');
+const stop = document.getElementById('stop');
+const reset = document.getElementById('reset');
+
+let intervalID;
+let startTime = 0;
+let elapsedTime = 0;
+
+//START BUTTON EVENT
+start.addEventListener('click', () => {
+    // If the timer is not running, set the start time to the current time
+    if (!intervalID) {
+        startTime = new Date().getTime() - elapsedTime;
+        intervalID = setInterval (() => {
+            elapsedTime = new Date().getTime() - startTime;
+            millisecondsDiplay.innerText = elapsedTime;
+        }, 1)
+    }
+});
+
+//STOP BUTTON EVENT
+stop.addEventListener('click', () => {
+    clearInterval(intervalID);
+    intervalID = null; // set the intervalID to null to indicate that the timer is not running
+});
+
+
+reset.addEventListener('click', () => {
+    elapsedTime = 0;
+    millisecondsDiplay.innerText = elapsedTime;
+})
+
+
+
+
+
+
+
+
+
+
+
